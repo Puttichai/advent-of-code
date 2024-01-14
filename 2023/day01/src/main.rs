d5f12d89a8d13a69cd8f6dbd262ddf6fc6cf835c
Petch <L.Puttichai@gmail.com>
2024-01-14 18:23:22 +0900

    Fixes indentation

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -2,154 +2,154 @@ use std::env;
 use std::fs;
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	let file_path = &args[1];
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
     println!("Reading file {file_path}");
 
-	let contents = fs::read_to_string(file_path)
-		.expect("File {file_path} is not valid");
-	let result1: u32 = part1(&contents);
-	println!("result1 = {result1}");
-	let result2: u32 = part2(&contents);
-	println!("result2 = {result2}");
+    let contents = fs::read_to_string(file_path)
+        .expect("File {file_path} is not valid");
+    let result1: u32 = part1(&contents);
+    println!("result1 = {result1}");
+    let result2: u32 = part2(&contents);
+    println!("result2 = {result2}");
 }
 
 fn part1(contents: &str) -> u32 {
-	const RADIX: u32 = 10;
-	let mut i1: usize; // index to the first digit in the line
-	let mut i2: usize; // index to the last digit in the line
-	let mut n1: u32;
-	let mut n2: u32;
-	let mut result: u32 = 0;
-	for line in contents.lines() {
-		// println!("{line}");
-		// Assume numbers always exist in each line
-		i1 = line.find(|c: char| c.is_numeric()).unwrap();
-		i2 = line.rfind(|c: char| c.is_numeric()).unwrap();
-		n1 = line.chars().nth(i1).unwrap()
-			.to_digit(RADIX).unwrap();
-		n2 = line.chars().nth(i2).unwrap()
-			.to_digit(RADIX).unwrap();
-		result += n1*10 + n2;
-	}
-	result
+    const RADIX: u32 = 10;
+    let mut i1: usize; // index to the first digit in the line
+    let mut i2: usize; // index to the last digit in the line
+    let mut n1: u32;
+    let mut n2: u32;
+    let mut result: u32 = 0;
+    for line in contents.lines() {
+        // println!("{line}");
+        // Assume numbers always exist in each line
+        i1 = line.find(|c: char| c.is_numeric()).unwrap();
+        i2 = line.rfind(|c: char| c.is_numeric()).unwrap();
+        n1 = line.chars().nth(i1).unwrap()
+            .to_digit(RADIX).unwrap();
+        n2 = line.chars().nth(i2).unwrap()
+            .to_digit(RADIX).unwrap();
+        result += n1*10 + n2;
+    }
+    result
 }
 
 fn part2(contents: &str) -> u32 {
-	let (mut i1, mut i2): (Option<usize>, Option<usize>);
-	let (mut r1, mut r2): (Option<u32>, Option<u32>);
-	let mut result: u32 = 0;
-	let mut value: u32; // temporary variable
-	for line in contents.lines() {
-		// Forward
-		(i1, r1) = get_index_and_value_word(line);
-		(i2, r2) = get_index_and_value_digit(line);
-		if i1.is_some() && i2.is_some() {
-			if i1 < i2 {
-				value = r1.unwrap();
-			}
-			else {
-				value = r2.unwrap();
-			}
-		}
-		else if i1.is_some() {
-			value = r1.unwrap();
-		}
-		else {
-			value = r2.unwrap();
-		}
-		result += value*10;
+    let (mut i1, mut i2): (Option<usize>, Option<usize>);
+    let (mut r1, mut r2): (Option<u32>, Option<u32>);
+    let mut result: u32 = 0;
+    let mut value: u32; // temporary variable
+    for line in contents.lines() {
+        // Forward
+        (i1, r1) = get_index_and_value_word(line);
+        (i2, r2) = get_index_and_value_digit(line);
+        if i1.is_some() && i2.is_some() {
+            if i1 < i2 {
+                value = r1.unwrap();
+            }
+            else {
+                value = r2.unwrap();
+            }
+        }
+        else if i1.is_some() {
+            value = r1.unwrap();
+        }
+        else {
+            value = r2.unwrap();
+        }
+        result += value*10;
 
-		// Backward
-		(i1, r1) = rget_index_and_value_word(line);
-		(i2, r2) = rget_index_and_value_digit(line);
-		if i1.is_some() && i2.is_some() {
-			if i1 > i2 {
-				value = r1.unwrap();
-			}
-			else {
-				value = r2.unwrap();
-			}
-		}
-		else if i1.is_some() {
-			value = r1.unwrap();
-		}
-		else {
-			value = r2.unwrap();
-		}
-		result += value;		
-	}
-	result
+        // Backward
+        (i1, r1) = rget_index_and_value_word(line);
+        (i2, r2) = rget_index_and_value_digit(line);
+        if i1.is_some() && i2.is_some() {
+            if i1 > i2 {
+                value = r1.unwrap();
+            }
+            else {
+                value = r2.unwrap();
+            }
+        }
+        else if i1.is_some() {
+            value = r1.unwrap();
+        }
+        else {
+            value = r2.unwrap();
+        }
+        result += value;
+    }
+    result
 }
 
 fn get_index_and_value_word(line: &str) -> (Option<usize>, Option<u32>) {
-	let numbers: [&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-	let mut index: usize = line.len();
-	let mut result: u32 = 0;
-	let mut i1: usize;
-	for (cur_index, strnumber) in numbers.iter().enumerate() {
-		i1 = match line.find(strnumber) {
-			Some(i) => i,
-			None => continue,
-		};
-		if i1 < index {
-			index = i1;
-			result = (cur_index as u32) + 1;
-		}
-	}
-	if result == 0 {
-		(None, None)
-	}
-	else {
-		(Some(index), Some(result))
-	}
+    let numbers: [&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let mut index: usize = line.len();
+    let mut result: u32 = 0;
+    let mut i1: usize;
+    for (cur_index, strnumber) in numbers.iter().enumerate() {
+        i1 = match line.find(strnumber) {
+            Some(i) => i,
+            None => continue,
+        };
+        if i1 < index {
+            index = i1;
+            result = (cur_index as u32) + 1;
+        }
+    }
+    if result == 0 {
+        (None, None)
+    }
+    else {
+        (Some(index), Some(result))
+    }
 }
 
 fn rget_index_and_value_word(line: &str) -> (Option<usize>, Option<u32>) {
-	let numbers: [&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-	let mut index: usize = 0;
-	let mut result: u32 = 0;
-	let mut has_result = false;
-	let mut i1: usize;
-	for (cur_index, strnumber) in numbers.iter().enumerate() {
-		i1 = match line.rfind(strnumber) {
-			Some(i) => i + strnumber.len() - (1 as usize), // index of the last letter in the word
-			None => continue,
-		};
-		has_result = true;
-		if i1 > index {
-			index = i1;
-			result = (cur_index as u32) + 1;
-		}
-	}
-	if !has_result {
-		(None, None)
-	}
-	else {
-		(Some(index), Some(result))
-	}
+    let numbers: [&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let mut index: usize = 0;
+    let mut result: u32 = 0;
+    let mut has_result = false;
+    let mut i1: usize;
+    for (cur_index, strnumber) in numbers.iter().enumerate() {
+        i1 = match line.rfind(strnumber) {
+            Some(i) => i + strnumber.len() - (1 as usize), // index of the last letter in the word
+            None => continue,
+        };
+        has_result = true;
+        if i1 > index {
+            index = i1;
+            result = (cur_index as u32) + 1;
+        }
+    }
+    if !has_result {
+        (None, None)
+    }
+    else {
+        (Some(index), Some(result))
+    }
 }
 
 fn get_index_and_value_digit(line: &str) -> (Option<usize>, Option<u32>) {
-	const RADIX: u32 = 10;
-	match line.find(|c: char| c.is_numeric()) {
-		Some(index) => {
-			let value: u32 = line.chars().nth(index).unwrap()
-				.to_digit(RADIX).unwrap();
-			(Some(index), Some(value))
-		},
-		None => (None, None),
-	}
+    const RADIX: u32 = 10;
+    match line.find(|c: char| c.is_numeric()) {
+        Some(index) => {
+            let value: u32 = line.chars().nth(index).unwrap()
+                .to_digit(RADIX).unwrap();
+            (Some(index), Some(value))
+        },
+        None => (None, None),
+    }
 }
 
 fn rget_index_and_value_digit(line: &str) -> (Option<usize>, Option<u32>) {
-	const RADIX: u32 = 10;
-	match line.rfind(|c: char| c.is_numeric()) {
-		Some(index) => {
-			let value: u32 = line.chars().nth(index).unwrap()
-				.to_digit(RADIX).unwrap();
-			(Some(index), Some(value))
-		},
-		None => (None, None),
-	}
+    const RADIX: u32 = 10;
+    match line.rfind(|c: char| c.is_numeric()) {
+        Some(index) => {
+            let value: u32 = line.chars().nth(index).unwrap()
+                .to_digit(RADIX).unwrap();
+            (Some(index), Some(value))
+        },
+        None => (None, None),
+    }
 }
--- 2023/day02/src/main.rs
+++ 2023/day02/src/main.rs
@@ -3,95 +3,95 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-	let file_path = &args[1];
-	println!("Reading file {file_path}");
+    let file_path = &args[1];
+    println!("Reading file {file_path}");
 
-	let contents = fs::read_to_string(file_path)
-		.expect("File {file_path} is not valid");
-	let result1: u32 = part1(&contents);
-	println!("result1 = {result1}");
-	let result2: u32 = part2(&contents);
-	println!("result2 = {result2}");
+    let contents = fs::read_to_string(file_path)
+        .expect("File {file_path} is not valid");
+    let result1: u32 = part1(&contents);
+    println!("result1 = {result1}");
+    let result2: u32 = part2(&contents);
+    println!("result2 = {result2}");
 }
 
 fn part1(contents: &str) -> u32 {
-	const NUM_RED: u32 = 12;
-	const NUM_GREEN: u32 = 13;
-	const NUM_BLUE: u32 = 14;
+    const NUM_RED: u32 = 12;
+    const NUM_GREEN: u32 = 13;
+    const NUM_BLUE: u32 = 14;
 
-	let mut is_possible: bool;
-	let mut sum_id: u32 = 0;
-	for line in contents.lines() {
-		// Each line is of the form
-		// Game X: i1 blue, i2 red, i3 green; ...
-		let v: Vec<&str> = line.split(":").collect();
-		let game_id_str: &str = &v[0][5..]; // v[0] is of the form "Game X"
-		// println!("{game_id_str}");
-		is_possible = analyze_game_samples(&v[1], NUM_RED, NUM_GREEN, NUM_BLUE);
-		if is_possible {
-			sum_id += game_id_str.parse::<u32>().unwrap();
-		}
-	}
-	sum_id
+    let mut is_possible: bool;
+    let mut sum_id: u32 = 0;
+    for line in contents.lines() {
+        // Each line is of the form
+        // Game X: i1 blue, i2 red, i3 green; ...
+        let v: Vec<&str> = line.split(":").collect();
+        let game_id_str: &str = &v[0][5..]; // v[0] is of the form "Game X"
+        // println!("{game_id_str}");
+        is_possible = analyze_game_samples(&v[1], NUM_RED, NUM_GREEN, NUM_BLUE);
+        if is_possible {
+            sum_id += game_id_str.parse::<u32>().unwrap();
+        }
+    }
+    sum_id
 }
 
 fn analyze_game_samples(samples: &str, num_red: u32, num_green: u32, num_blue: u32) -> bool {
-	for sample in samples.trim().split(";") {
-		for set in sample.trim().split(",") {
-			let (number_str, color_str) = set.trim().split_once(" ").unwrap();
-			let num_cubes: u32 = number_str.parse::<u32>().unwrap();
-			if color_str == "red" && num_cubes > num_red {
-				return false;
-			}
-			else if color_str == "green" && num_cubes > num_green {
-				return false;
-			}
-			else if color_str == "blue" && num_cubes > num_blue {
-				return false;
-			}
-		}
-	}
-	true
+    for sample in samples.trim().split(";") {
+        for set in sample.trim().split(",") {
+            let (number_str, color_str) = set.trim().split_once(" ").unwrap();
+            let num_cubes: u32 = number_str.parse::<u32>().unwrap();
+            if color_str == "red" && num_cubes > num_red {
+                return false;
+            }
+            else if color_str == "green" && num_cubes > num_green {
+                return false;
+            }
+            else if color_str == "blue" && num_cubes > num_blue {
+                return false;
+            }
+        }
+    }
+    true
 }
 
 fn part2(contents: &str) -> u32 {
-	let mut power: u32 = 0;
-	for line in contents.lines() {
-		let v: Vec<&str> = line.split(":").collect();
-		power += compute_power(&v[1]);
-	}
-	power
+    let mut power: u32 = 0;
+    for line in contents.lines() {
+        let v: Vec<&str> = line.split(":").collect();
+        power += compute_power(&v[1]);
+    }
+    power
 }
 
 fn compute_power(samples: &str) -> u32 {
-	let mut min_red: u32 = 0;
-	let mut min_green: u32 = 0;
-	let mut min_blue: u32 = 0;
-	for sample in samples.trim().split(";") {
-		for set in sample.trim().split(",") {
-			let (number_str, color_str) = set.trim().split_once(" ").unwrap();
-			let num_cubes: u32 = number_str.parse::<u32>().unwrap();
-			match color_str {
-				"red" => {
-					if num_cubes > min_red {
-						min_red = num_cubes;
-					}
-				},
-				"green" => {
-					if num_cubes > min_green {
-						min_green = num_cubes;
-					}
-				},
-				"blue" => {
-					if num_cubes > min_blue {
-						min_blue = num_cubes;
-					}
-				},
-				&_ => {
-					// Nothing to do
-				},
-			}
-		}
-	}
-	min_red*min_green*min_blue
+    let mut min_red: u32 = 0;
+    let mut min_green: u32 = 0;
+    let mut min_blue: u32 = 0;
+    for sample in samples.trim().split(";") {
+        for set in sample.trim().split(",") {
+            let (number_str, color_str) = set.trim().split_once(" ").unwrap();
+            let num_cubes: u32 = number_str.parse::<u32>().unwrap();
+            match color_str {
+                "red" => {
+                    if num_cubes > min_red {
+                        min_red = num_cubes;
+                    }
+                },
+                "green" => {
+                    if num_cubes > min_green {
+                        min_green = num_cubes;
+                    }
+                },
+                "blue" => {
+                    if num_cubes > min_blue {
+                        min_blue = num_cubes;
+                    }
+                },
+                &_ => {
+                    // Nothing to do
+                },
+            }
+        }
+    }
+    min_red*min_green*min_blue
 }
