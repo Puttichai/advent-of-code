7302a275dade653fef3e69b652991df538468c3d
Petch <L.Puttichai@gmail.com>
2024-02-04 09:57:19 +0900

    Adds day08 part 2

--- 2023/day08/src/main.rs
+++ 2023/day08/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::collections::HashMap;
+use std::time::Instant;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,10 +19,17 @@ fn main() {
         })
         .collect();
     println!("instructions={:?}", instructions);
-
     let network: HashMap<&str, (&str, &str)> = extract_network(&contents);
+
+    let now1 = Instant::now();
     let result1: u64 = part1(&instructions, &network);
-    println!("result1 = {result1}");
+    let elapsed1 = now1.elapsed();
+    println!("result1 = {result1} computed in {:?}", elapsed1);
+
+    let now2 = Instant::now();
+    let result2: u64 = part2(&instructions, &network);
+    let elapsed2 = now2.elapsed();
+    println!("result2 = {result2} computed in {:?}", elapsed2);
 }
 
 fn extract_network(contents: &str) -> HashMap<&str, (&str, &str)> {
@@ -59,7 +67,7 @@ fn part1(instructions: &Vec<u64>, network: &HashMap<&str, (&str, &str)>) -> u64 
             Some(key_value) => {
                 let choices: &(&str, &str) = key_value.1;
                 let next_node: &str = if instructions[current_instruction_index] == 0 { choices.0 } else { choices.1 };
-                println!("step {num_steps}: current node = {current_node}, next node = {next_node}");
+                // println!("step {num_steps}: current node = {current_node}, next node = {next_node}");
                 if next_node == dest_node {
                     break;
                 }
@@ -70,10 +78,52 @@ fn part1(instructions: &Vec<u64>, network: &HashMap<&str, (&str, &str)>) -> u64 
                 }
             },
             None => {
-                println!("Cannot find entry {current_node} in the network so terminating.");
+                println!("[part1] Cannot find entry {current_node} in the network so terminating.");
                 return 0;
             },
         }
     }
     num_steps
+}
+
+fn part2(instructions: &Vec<u64>, network: &HashMap<&str, (&str, &str)>) -> u64 {
+    let mut current_state: Vec<&str> = vec![];
+    for key in network.keys() {
+        if key.chars().last().unwrap() == 'A' {
+            current_state.push(key);
+        }
+    }    
+    current_state.sort();
+    
+    // Assume that going from a node '--A' to a node '--Z' consumes the entire instructions N times.
+    let mut num_cycles: Vec<u64> = vec![];
+    let num_instructions: usize = instructions.len();
+
+    for node in &current_state {
+        let mut current_instruction_index: usize = 0;
+        let mut num_steps: u64 = 0;
+        let mut current_node: &str = node;
+        loop {
+            num_steps += 1;
+            let current_instruction: u64 = instructions[current_instruction_index];
+            let choices: &(&str, &str) = network.get_key_value(current_node).unwrap().1;
+            let next_node: &str = if current_instruction == 0 {choices.0} else {choices.1};
+            if next_node.chars().last().unwrap() == 'Z' {
+                break;
+            }
+
+            current_node = next_node;
+            current_instruction_index += 1_usize;
+            if current_instruction_index >= num_instructions {
+                current_instruction_index = 0;
+            }
+        }
+        num_cycles.push(num_steps / (num_instructions as u64));
+    }
+    println!("num_cycles={:?}", num_cycles);
+    
+    num_cycles
+        .iter()
+        .fold(num_cycles[0],
+              |acc, num_cycle| num_integer::lcm(acc, *num_cycle)) * (num_instructions as u64)
 }
